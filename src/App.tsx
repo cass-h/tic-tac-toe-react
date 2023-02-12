7f0748e92f745130ee5bcbeb5c13ccf0c6297547
cass-h <119467824+cass-h@users.noreply.github.com>
2023-02-12 09:08:27 -0500

    game functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Board } from "./Board/Board";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Board xStart={true} />
     </div>
   );
 }
--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Board.module.css";
 
 export function Board(props: { xStart: boolean }) {
@@ -9,12 +9,71 @@ export function Board(props: { xStart: boolean }) {
 
     After each turn has passed, we will evaluate if the game is complete.
   */
-  const [board, setBoard] = useState([
+  type CellType = boolean | null;
+  type BoardType = CellType[][];
+
+  const [board, setBoard] = useState<BoardType>([
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ]);
 
+  const [gameOver, setGameOver] = useState<boolean>(false);
+
   const [player, setPlayer] = useState(props.xStart);
-  return <div className={styles.grid}></div>;
+
+  function clickTile(x: number, y: number) {
+    board[x][y] = player;
+    setPlayer(!player);
+  }
+  function getLabel(value: boolean | null) {
+    switch (value) {
+      case true:
+        return "X";
+      case false:
+        return "O";
+      case null:
+        return "-";
+    }
+  }
+
+  function checkTiles(...cells: CellType[]) {
+    if (cells[0] !== null)
+      if (cells.every((cell) => cell === cells[0])) {
+        console.log(cells[0], " Wins");
+        setGameOver(true);
+      }
+  }
+
+  useEffect(() => {
+    // check horizontals
+    board.forEach((row) => {
+      checkTiles(row[0], row[1], row[2]);
+    });
+    // check verticals
+    board[0].forEach((column, y) => {
+      checkTiles(board[0][y], board[1][y], board[2][y]);
+    });
+    // check diagonals
+    checkTiles(board[0][0], board[1][1], board[2][2]);
+    checkTiles(board[0][2], board[1][1], board[2][0]);
+  }, [player, board]);
+  return (
+    <div className={styles.grid}>
+      {board.map((row, x) =>
+        row.map((column, y) => {
+          return (
+            <button
+              className={styles.button}
+              key={`${x},${y}`}
+              onClick={() => clickTile(x, y)}
+              disabled={board[x][y] !== null || gameOver}
+            >
+              {getLabel(board[x][y])}
+            </button>
+          );
+        })
+      )}
+    </div>
+  );
 }
